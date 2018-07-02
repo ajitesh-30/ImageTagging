e888eae934c84b27efd86e7703f0bf7794627086
ks692 <ks692@snu.edu.in>
2018-07-03 01:51:05 +0530

    Update classFeatureExtraction(2).py

--- Code/classFeatureExtraction(2).py
+++ Code/classFeatureExtraction(2).py
@@ -1,43 +1,56 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sat Jun 30 14:50:52 2018
+Created on Mon Jul  2 18:04:55 2018
 
 @author: HULK
 """
 
-import cv2
-import numpy as np
-import matplotlib.pyplot as plt
-from skimage.io import imread
-from skimage.feature import hog
-from skimage import data ,exposure
-from PIL import Image
+import nltk
+from nltk.corpus import wordnet
+
+#find synonyms
+def findSynonyms(word):
+    synonyms = []
+    for syn in wordnet.synsets(word):
+        for l in syn.lemmas():
+            synonyms.append(l.name())
+    return synonyms               
+
+#find similarity between 2 words check and append if it is noun or verb
+def findSimilarity(word1,word2):
+     verbs = {x.name().split('.', 1)[0] for x in wordnet.all_synsets('v')}
+     if(word1 in verbs):
+         word1+=(".v.01")
+     else:
+         word1+=(".n.01")
+     if(word2 in verbs):
+         word2+=(".v.01")
+     else:
+         word2+=(".n.01")
+         
+     w1 = wordnet.synset(word1)
+     w2 = wordnet.synset(word2)     
+     return w1.wup_similarity(w2)
+ 
+def Average(list1):
+    return sum(list1) / len(list1)
+     
+#returns accuracy list1 is predicted list 2 is original
+ 
+def accuracy(list1,list2):
+    finalList=[]
+    for word1 in list1:
+        listcheck=[]
+        for word2 in list2:
+            k=findSimilarity(word1,word2)
+            listcheck.append(k)
+        finalList.append(min(listcheck))
+    return 1-Average(finalList)
 
 
-def siftExtractor(imgPath):
-    image = cv2.imread(imgPath)
-    img = cv2.resize(image,(256,256),interpolation = cv2.INTER_AREA)
-    gray= cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-    sift= cv2.xfeatures2d.SIFT_create(400)
-    kp, des = sift.detectAndCompute(gray,None)
-    return kp,des
-    
-def hogExtractor(imgPath):
-    image = cv2.imread(imgPath)
-    img = cv2.resize(image,(256,256),interpolation = cv2.INTER_AREA)
-    temp=np.asarray(img)
-    x=temp.shape[0]
-    y=temp.shape[1]*temp.shape[2]
-    temp.resize((x,y)) 
-    image=temp
-    fd, hog_image = hog(image, orientations=8, pixels_per_cell=(16, 16), cells_per_block=(1, 1), transform_sqrt=True, block_norm="L2", visualise=True)
-    return fd, hog_image
 
-def surfExtractor(imgPath):
-    img = cv2.imread(imgPath)
-    surf =cv2.xfeatures2d.SURF_create(400)
-    surf.setHessianThreshold(1000)
-    surf.setExtended(1)
-    kp, des = surf.detectAndCompute(img,None)
-    return kp,des
-        
+print(findSynonyms("love"))
+list1=["actor"]
+list2=["acting"]
+
+print(accuracy(list1,list2))
